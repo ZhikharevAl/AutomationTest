f6bfa16627665bee35dea0b799bb5f273de16df2
AlexeyZh <waltafunk@gmail.com>
2022-06-20 22:25:43 +0300

    add def check_filled_form

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -12,3 +12,11 @@ class TextBoxPage(BasePage):
         self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys('456 Main St')
         self.element_is_visible(self.locators.SUBMIT).click()
 
+    def check_filled_form(self):
+        full_name = self.element_is_present(self.locators.CREATED_FULL_NAME).text.split(':')[1]
+        email = self.element_is_present(self.locators.CREATED_EMAIL).text.split(':')[1]
+        current_address = self.element_is_present(self.locators.CREATED_CURRENT_ADDRESS).text.split(':')[1]
+        permanent_address = self.element_is_present(self.locators.CREATED_PERMANENT_ADDRESS).text.split(':')[1]
+        return full_name, email, current_address, permanent_address
+
+
