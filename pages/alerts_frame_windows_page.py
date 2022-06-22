c7efc97873a4d4c38b9206e22633af3aa0d29ef3
AlexeyZh <waltafunk@gmail.com>
2022-06-22 21:05:08 +0300

    add class AlertsPage

--- pages/alerts_frame_windows_page.py
+++ pages/alerts_frame_windows_page.py
@@ -3,23 +3,56 @@ import time
 
 from selenium.common.exceptions import UnexpectedAlertPresentException
 
-from locators.alerts_frame_windows_locators import BrowserWindowsPageLocators
+from locators.alerts_frame_windows_locators import BrowserWindowsPageLocators, AlertsPageLocators
 from pages.base_page import BasePage
 
 
 class BrowserWindowsPage(BasePage):
-    locators = BrowserWindowsPageLocators()
+    locators=BrowserWindowsPageLocators()
 
     def check_opened_new_tab(self):
         self.element_is_visible(self.locators.NEW_TAB_BUTTON).click()
         self.driver.switch_to.window(self.driver.window_handles[1])
-        text_title = self.element_is_present(self.locators.TITLE_NEW).text
+        text_title=self.element_is_present(self.locators.TITLE_NEW).text
         return text_title
 
     def check_opened_new_window(self):
         self.element_is_visible(self.locators.NEW_WINDOW_BUTTON).click()
         self.driver.switch_to.window(self.driver.window_handles[1])
-        text_title = self.element_is_present(self.locators.TITLE_NEW).text
+        text_title=self.element_is_present(self.locators.TITLE_NEW).text
         return text_title
 
 
+class AlertsPage(BasePage):
+    locators=AlertsPageLocators()
+
+    def check_see_alert(self):
+        self.element_is_visible(self.locators.SEE_ALERT_BUTTON).click()
+        alert_window=self.driver.switch_to.alert
+        return alert_window.text
+
+    def check_alert_appear_5_sec(self):
+        self.element_is_visible(self.locators.APPEAR_ALERT_AFTER_5_SEC_BUTTON).click()
+        time.sleep(6)
+        try:
+            alert_window=self.driver.switch_to.alert
+            return alert_window.text
+        except UnexpectedAlertPresentException:
+            alert_window=self.driver.switch_to.alert
+            return alert_window.text
+
+    def check_confirm_alert(self):
+        self.element_is_visible(self.locators.CONFIRM_BOX_ALERT_BUTTON).click()
+        alert_window=self.driver.switch_to.alert
+        alert_window.accept()
+        text_result=self.element_is_present(self.locators.CONFIRM_RESULT).text
+        return text_result
+
+    def check_prompt_alert(self):
+        text=f"autotest{random.randint(0, 999)}"
+        self.element_is_visible(self.locators.PROMPT_BOX_ALERT_BUTTON).click()
+        alert_window=self.driver.switch_to.alert
+        alert_window.send_keys(text)
+        alert_window.accept()
+        text_result=self.element_is_present(self.locators.PROMPT_RESULT).text
+        return text, text_result
