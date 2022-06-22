87894ab00fd7b36c871c2779a812215f87842b91
AlexeyZh <waltafunk@gmail.com>
2022-06-22 18:28:28 +0300

    update

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,17 +1,18 @@
+from selenium.webdriver import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver import ActionChains
 
 
 class BasePage:
     def __init__(self, driver, url):
-        self.driver = driver
-        self.url = url
+        self.driver=driver
+        self.url=url
 
     def open(self):
         self.driver.get(self.url)
 
     def element_is_visible(self, locator, timeout=5):
+        self.go_to_element(self.element_is_present(locator))
         return wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
 
     def elements_are_visible(self, locator, timeout=5):
@@ -33,11 +34,26 @@ class BasePage:
         self.driver.execute_script("arguments[0].scrollIntoView();", element)
 
     def action_double_click(self, element):
-        action = ActionChains(self.driver)
+        action=ActionChains(self.driver)
         action.double_click(element)
         action.perform()
 
     def action_right_click(self, element):
-        action = ActionChains(self.driver)
+        action=ActionChains(self.driver)
         action.context_click(element)
+        action.perform()
+
+    def action_drag_and_drop_by_offset(self, element, x_coords, y_coords):
+        action=ActionChains(self.driver)
+        action.drag_and_drop_by_offset(element, x_coords, y_coords)
+        action.perform()
+
+    def action_drag_and_drop_to_element(self, what, where):
+        action=ActionChains(self.driver)
+        action.drag_and_drop(what, where)
+        action.perform()
+
+    def action_move_to_element(self, element):
+        action=ActionChains(self.driver)
+        action.move_to_element(element)
         action.perform()
