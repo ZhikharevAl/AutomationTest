89976d24aaefec4766e44520888ef99132d8b046
AlexeyZh <waltafunk@gmail.com>
2022-06-23 17:23:00 +0300

    add class TestToolTipsPage

--- tests/widgets_test.py
+++ tests/widgets_test.py
@@ -1,7 +1,7 @@
 import time
 
 from pages.widgets_page import AccordianPage, AutoCompletePage, DatePickerPage, SliderPage, ProgressBarPage, \
-    TabsPage
+    TabsPage, ToolTipsPage
 
 
 class TestWidgets:
@@ -73,8 +73,23 @@ class TestWidgets:
             what_button, what_content=tabs.check_tabs('what')
             origin_button, origin_content=tabs.check_tabs('origin')
             use_button, use_content=tabs.check_tabs('use')
-            #more_button, more_content=tabs.check_tabs('more')
+            # more_button, more_content=tabs.check_tabs('more')
             assert what_button == 'What' and what_content != 0, 'the tab "what" was not pressed or the text is missing'
             assert origin_button == 'Origin' and origin_content != 0, 'the tab "origin" was not pressed or the text is missing'
             assert use_button == 'Use' and use_content != 0, 'the tab "use" was not pressed or the text is missing'
-            #assert more_button == 'More' and what_content != 0, 'the tab "more" was not pressed or the text is missing'
+            # assert more_button == 'More' and what_content != 0, 'the tab "more" was not pressed or the text is missing'
+
+    class TestToolTips:
+        def test_tool_tips(self, driver):
+            tool_tips_page=ToolTipsPage(driver, 'https://demoqa.com/tool-tips')
+            tool_tips_page.open()
+            button_text, field_text, contrary_text, section_text=tool_tips_page.check_tool_tips()
+            #assert button_text == 'You hovered over the Button', 'hover missing or incorrect content'
+            #assert field_text == 'You hovered over the text field', 'hover missing or incorrect content'
+            #assert contrary_text == 'You hovered over the Contrary', 'hover missing or incorrect content'
+            #assert section_text == 'You hovered over the 1.10.32', 'hover missing or incorrect content'
+
+
+
+
+
