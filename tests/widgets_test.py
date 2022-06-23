1a5d6a05daab80ec96d130bda4832d52f860c88e
AlexeyZh <waltafunk@gmail.com>
2022-06-23 13:20:08 +0300

    add class TestDatePickerPage

--- tests/widgets_test.py
+++ tests/widgets_test.py
@@ -1,40 +1,52 @@
 import time
 
-
-from pages.widgets_page import AccordianPage, AutoCompletePage
+from pages.widgets_page import AccordianPage, AutoCompletePage, DatePickerPage
 
 
 class TestWidgets:
     class TestAccordianPage:
 
         def test_accordian(self, driver):
-            accordian_page = AccordianPage(driver, 'https://demoqa.com/accordian')
+            accordian_page=AccordianPage(driver, 'https://demoqa.com/accordian')
             accordian_page.open()
-            first_title, first_content = accordian_page.check_accordian('first')
-            second_title, second_content = accordian_page.check_accordian('second')
-            third_title, third_content = accordian_page.check_accordian('third')
+            first_title, first_content=accordian_page.check_accordian('first')
+            second_title, second_content=accordian_page.check_accordian('second')
+            third_title, third_content=accordian_page.check_accordian('third')
             assert first_title == 'What is Lorem Ipsum?' and first_content > 0, 'Incorrect title or missing text'
             assert second_title == 'Where does it come from?' and second_content > 0, 'Incorrect title or missing text'
             assert third_title == 'Why do we use it?' and third_content > 0, 'Incorrect title or missing text'
 
     class TestAutoCompletePage:
         def test_fill_multi_autocomplete(self, driver):
-            autocomplete_page = AutoCompletePage(driver, 'https://demoqa.com/auto-complete')
+            autocomplete_page=AutoCompletePage(driver, 'https://demoqa.com/auto-complete')
             autocomplete_page.open()
-            colors = autocomplete_page.fill_input_multi()
-            colors_result = autocomplete_page.check_color_in_multi()
+            colors=autocomplete_page.fill_input_multi()
+            colors_result=autocomplete_page.check_color_in_multi()
             assert colors == colors_result, 'the added colors are missing in the input'
 
         def test_remove_value_from_multi(self, driver):
-            autocomplete_page = AutoCompletePage(driver, 'https://demoqa.com/auto-complete')
+            autocomplete_page=AutoCompletePage(driver, 'https://demoqa.com/auto-complete')
             autocomplete_page.open()
             autocomplete_page.fill_input_multi()
-            count_value_before, count_value_after = autocomplete_page.remove_value_from_multi()
+            count_value_before, count_value_after=autocomplete_page.remove_value_from_multi()
             assert count_value_before != count_value_after, "value was not deleted"
 
         def test_fill_single_autocomplete(self, driver):
-            autocomplete_page = AutoCompletePage(driver, 'https://demoqa.com/auto-complete')
+            autocomplete_page=AutoCompletePage(driver, 'https://demoqa.com/auto-complete')
             autocomplete_page.open()
-            color = autocomplete_page.fill_input_single()
-            color_result = autocomplete_page.check_color_in_single()
+            color=autocomplete_page.fill_input_single()
+            color_result=autocomplete_page.check_color_in_single()
             assert color == color_result, 'the added colors are missing in the input'
+
+    class TestDatePickerPage:
+        def test_change_date(self, driver):
+            date_picker_page=DatePickerPage(driver, 'https://demoqa.com/date-picker')
+            date_picker_page.open()
+            value_date_before, value_date_after=date_picker_page.select_date()
+            assert value_date_before != value_date_after, 'the date has not been changed'
+
+        def test_change_date_and_time(self, driver):
+            date_picker_page=DatePickerPage(driver, 'https://demoqa.com/date-picker')
+            date_picker_page.open()
+            value_date_before, value_date_after=date_picker_page.select_date_and_time()
+            assert value_date_before != value_date_after, 'the date and time have not been changed'
